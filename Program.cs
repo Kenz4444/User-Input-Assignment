3e008526f0c61f6d0849d44ce0807bc0de151a35
Mackenzie McIntyre <362574782@GoTVDSB.ca>
2026-02-13 13:19:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace User_Input_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+
+        public static void Part1()
+        {
+            string usersName;
+            int usersAge;
+            Console.Write("Hello! Whhats your name? ");
+            Console.Write("");
+            usersName = Console.ReadLine();
+            Console.WriteLine("Oh nice to meet you " + usersName + ", my name is Kenzie!");
+
+
+        }
+    }
+}
